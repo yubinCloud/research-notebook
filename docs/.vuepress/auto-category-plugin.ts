4e54ce72c44dbce98f71d61c8c0962278e9814e9
yubinCloud <yubin_SkyWalker@yeah.net>
2024-06-08 15:01:38 +0800

    增加 PET-SQL

--- docs/.vuepress/auto-category-plugin.ts
+++ docs/.vuepress/auto-category-plugin.ts
@@ -1,26 +1,51 @@
 import { App, createPage, Page, PluginConfig } from 'vuepress'
+import fs from 'node:fs'
 
+const frontmatters = {}
 
 export const autoCategoryPlugin = (app: App) => {
     return {
         name: 'auto-category-plugin',
         
         extendsPage: (page: Page) => {
-            if (page.permalink != undefined && page.permalink.startsWith("/category")) {
-                if (page.sfcBlocks.template != undefined) {
-                    page.sfcBlocks.template.contentStripped = page.sfcBlocks.template?.contentStripped + '\nHello World'
-                }
-            }
+            frontmatters[page.slug] = page.frontmatter
         },
         
         async onInitialized(app: App) {
-            const categorys: Array<any> = []
-            app.pages.forEach(page => {
-                if (page.permalink != undefined && page.permalink.startsWith("/category")) {
-                    console.log(page)
-                    categorys.push(page)
+
+            console.log(frontmatters)
+
+
+            for (let page of app.pages) {
+                if (page.permalink != undefined && page.permalink.startsWith('/category')) {
+                    const notesDir = page.pathInferred  // notes 所在的目录
+                    if (notesDir === null) {
+                        return
+                    }
+                    const noteFiles = fs.readdirSync("./docs/" + notesDir)
+                    if (page.permalink === "/category/Text2SQL/") {
+                        console.log(noteFiles)
+                    }
+                    var tableRows: string[] = []
+                    var tableHeader = `<tr><th style="text-align: center;">Title</th><th style="text-align: center;">Link</th></tr>`
+                    for (let noteFile of noteFiles) {
+                        // 一个 note 的信息
+                        const fname = noteFile.substring(0, noteFile.length - 3)
+                        if (fname === 'README') {
+                            continue
+                        }
+                        const frontmatter = frontmatters[fname]
+                        var tableRow = `<tr><td>${frontmatter.title}</td><td><a href="/research-notebook${frontmatter.permalink}">${frontmatter.permalink}</a></td></tr>`
+                        tableRows.push(tableRow)
+                    }
+                    const tableContent = tableRows.join('\n')
+                    const tableHTML = `<table>\n${tableHeader}\n${tableContent}\n</table>`
+                    if (page.sfcBlocks.template != undefined) {
+                        page.sfcBlocks.template.contentStripped = page.sfcBlocks.template?.contentStripped + `<h2 id="notesTable"><span>目录导航：</span></h2>\n` + tableHTML
+                    }
                 }
-            })
+            }
+
             const categoryPage = await createPage(app, {
                 path: '/category/my/',
                 frontmatter: {
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,22 @@
 ---
 home: true
+config:
+  -
+    type: hero
+    full: true
+    background: tint-plate
+    tintPlate:
+      r:
+        value: 167
+        offset: 34
+      g:
+        value: 167
+        offset: 36
+      b:
+        value: 223
+        offset: 33
+    hero:
+      name: Research Notebook
+      tagline: 风带来故事的种子
+      text: LLM & RAG & Text2SQL
 ---
